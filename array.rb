38b90ff673ea1fb429bfda6ec0a23b2e70ee81b9
amyma17 <amyma171311@gmail.com>
2018-06-20 21:32:43 +0000

    automatically backed up by learn

---
+++ array.rb
@@ -0,0 +1,11 @@
+# books = ["Everything Everything",
+# "Ready Player One",
+# "Red Queen",
+# "Carry on"]
+# puts books
+
+disney = ["Maleficent","Mufasa","Mulan","Moana","Mike"]
+
+disney.push("Pooh","tigger")
+
+puts disney.length
